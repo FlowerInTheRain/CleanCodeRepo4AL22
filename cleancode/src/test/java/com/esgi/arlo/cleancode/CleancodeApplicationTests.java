fe74b9de278564d5521a17e7540857bd64cc4f8e
Sid-Athos <sa.bennaceur@gmail.com>
2022-10-18 15:47:09 +0200

    CC-3 create springApp

---
+++ cleancode/src/test/java/com/esgi/arlo/cleancode/CleancodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.arlo.cleancode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleancodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
