16b32d76f8064bf6b286d480fbb83f5ab17b4029
Sid-Athos <sa.bennaceur@gmail.com>
2022-10-20 01:28:59 +0200

    properties

--- cleancode/clean-code-api/src/main/java/com/cleancode/cleancodeapi/beans/user/UserName.java
+++ cleancode/clean-code-api/src/main/java/com/cleancode/cleancodeapi/beans/user/UserName.java
@@ -1,5 +1,10 @@
 package com.cleancode.cleancodeapi.beans.user;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 public class UserName {
     private final String firstName;
     private final String lastName;
@@ -23,5 +28,13 @@ public class UserName {
 
     public static UserName createOne(String firstName, String lastName){
         return new UserName(firstName, lastName);
+    }
+    // Optional shouldn't be sent. We move the if present to the caller and take a map into argument
+    public static List<UserName> createMultiple(Optional<Map<String, String>> firstNameAndLaseNameMap){
+        List<UserName> createdUserNameList = new ArrayList<>();
+        firstNameAndLaseNameMap.ifPresent(stringStringMap -> stringStringMap.forEach((firstName, lastName) -> {
+            createdUserNameList.add(createOne(firstName, lastName));
+        }));
+        return createdUserNameList;
     }
 }
