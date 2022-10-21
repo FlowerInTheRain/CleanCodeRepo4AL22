dd585eba8d75bf2d00dddd27ff0919c816efadda
Sid-Athos <sa.bennaceur@gmail.com>
2022-10-21 20:48:53 +0200

    keywords modif in comment + factorize imports

--- cleancode/clean-code-db-impl/src/test/java/cleancodetests/ut/impl/user/UserServiceImplUT.java
+++ cleancode/clean-code-db-impl/src/test/java/cleancodetests/ut/impl/user/UserServiceImplUT.java
@@ -1,25 +1,20 @@
 package cleancodetests.ut.impl.user;
 
 // Packages
+
 import com.esgi.arlo.entities.users.UsersEntity;
 import com.esgi.arlo.repositories.user.UserRepository;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 
-// Static methods
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.atMostOnce;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {UsersEntity.class})
-@AutoConfigureMockMvc
 public class UserServiceImplUT {
 
     @Mock
@@ -39,6 +34,6 @@ public class UserServiceImplUT {
 
         // CHECK
         verify(userRepository, atMostOnce()).save(mockedUserEntity);
-        Assert.isTrue(usersEntityToReturn.getId().equals(returnedUserEntity.getId()));
+        Assertions.assertEquals(usersEntityToReturn.getId(), returnedUserEntity.getId());
     }
 }
