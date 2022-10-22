90cbf54ac3875de1edae36154cfbf942b25ceabe
Sid-Athos <sa.bennaceur@gmail.com>
2022-10-22 10:46:13 +0200

    yop

--- cleancode/clean-code-db-impl/src/main/java/com/cleancode/cleancodedbimpl/generators/formatters/UUIDFormatter.java
+++ cleancode/clean-code-db-impl/src/main/java/com/cleancode/cleancodedbimpl/generators/formatters/UUIDFormatter.java
@@ -1,10 +1,15 @@
 package com.cleancode.cleancodedbimpl.generators.formatters;
 
+import com.cleancode.cleancodedbimpl.generators.UUIDGenerator;
+
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UUIDFormatter {
+    private static final Logger LOGGER = Logger.getLogger(UUIDFormatter.class.getName());
     public static Optional<String> formatUUIDSequenceWithPrefixAndSuffixOptions(UUID uuidToFormat, boolean withUnionTrailRemoval, CharSequence joiner, boolean withPrefix,
                                                                             String prefix,
                                                       boolean withSuffix, String suffix){
@@ -25,20 +30,21 @@ public class UUIDFormatter {
             return Optional.ofNullable(newBusinessReferenceToBindWithoutUnionTrails);
         }
 
-        return Optional.of(String.join(Objects.requireNonNullElse(joiner, ""), newBusinessReferenceToBindWithoutUnionTrails));
+        return Optional.of(String.join(Objects.requireNonNullElse(joiner, ""), newBusinessReferenceToBindWithoutUnionTrails.split("-")));
     }
 
     public static Optional<String> formatUUIDSequence(UUID uuidToFormat, boolean withUnionTrailRemoval, CharSequence joiner){
         if(uuidToFormat == null){
             throw new IllegalArgumentException("UUID can not be null");
         }
-
+        LOGGER.log(Level.INFO,
+                "Formatting UUIDSequence. UUID : ".concat(uuidToFormat.toString()).concat(" SplitOption : ").concat(withUnionTrailRemoval ? "true":"false").concat(" Joiner : ").concat(joiner.toString()));
         String newBusinessReferenceToBindWithoutUnionTrails = uuidToFormat.toString();
 
         if(!withUnionTrailRemoval){
             return Optional.ofNullable(newBusinessReferenceToBindWithoutUnionTrails);
         }
 
-        return Optional.of(String.join(Objects.requireNonNullElse(joiner, ""), newBusinessReferenceToBindWithoutUnionTrails));
+        return Optional.of(String.join(joiner, newBusinessReferenceToBindWithoutUnionTrails.split("-")));
     }
 }
