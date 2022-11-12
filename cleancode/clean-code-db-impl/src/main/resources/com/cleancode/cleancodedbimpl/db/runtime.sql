856e4355a2108804f7ad084956d836376f5c1950
Arnaud <arnaudjourdain@orange.fr>
2022-11-12 17:07:34 +0100

    runtime sql

---
+++ cleancode/clean-code-db-impl/src/main/resources/com/cleancode/cleancodedbimpl/db/runtime.sql
@@ -0,0 +1,2 @@
+INSERT INTO PUBLIC.card_entity(cardReference, cardCollectionReference, cardRarity, cardSpecialty, cardName, xp, level) VALUES
+    ('card1', 'collection1', 'LEGENDARY', 'MAGE', 'ARNAUD', 0, 1);
