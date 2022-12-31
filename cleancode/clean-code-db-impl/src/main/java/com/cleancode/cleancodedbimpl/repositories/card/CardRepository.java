04b99727000fa62d1adc5f79f2cbbbfd33bd820f
Arnaud <arnaudjourdain@orange.fr>
2022-12-31 14:59:14 +0100

    Test Card Repository

--- cleancode/clean-code-db-impl/src/main/java/com/cleancode/cleancodedbimpl/repositories/card/CardRepository.java
+++ cleancode/clean-code-db-impl/src/main/java/com/cleancode/cleancodedbimpl/repositories/card/CardRepository.java
@@ -4,8 +4,6 @@ import com.cleancode.cleancodedbimpl.entities.cards.CardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CardRepository extends JpaRepository<CardEntity, Long> {
 
