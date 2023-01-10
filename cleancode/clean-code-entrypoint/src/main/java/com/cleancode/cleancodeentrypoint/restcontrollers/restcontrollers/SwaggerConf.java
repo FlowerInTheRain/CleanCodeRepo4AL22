4e856f4c15a2612a67d45f040251843f2f73a7d6
Sid-Athos <sa.bennaceur@gmail.com>
2023-01-10 17:49:59 +0100

    Adding specific swagger conf

---
+++ cleancode/clean-code-entrypoint/src/main/java/com/cleancode/cleancodeentrypoint/restcontrollers/restcontrollers/SwaggerConf.java
@@ -0,0 +1,9 @@
+package com.cleancode.cleancodeentrypoint.restcontrollers.restcontrollers;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.cleancode.cleancodeentrypoint.restcontrollers")
+public class SwaggerConf {
+}
